703336cdc32a52a007a9c9e751092d97ea4ddad3
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-05-02 15:31:03 -0600

    Create horizontal gallery
    Create horizontal gallery of images but needs js, to do it more dinamic

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var galleryItems = $('.gallery li');
+for (var i = 0; i < galleryItems.length; i+=2) {
+    console.log(galleryItems[i]);
+};
+
+$("body").mousewheel(function(event, delta) {
+    this.scrollLeft -= (delta * 30);
+    event.preventDefault();
+}); 
+// galleryItems.each(function(index, item){
+//     console.log(galleryItems);
+// });
